da43baef77988bccca1ab3669d583abc77eb01ce
tricolour <tricolourproit@gmail.com>
2020-10-27 18:26:23 +0530

    Add Basic Arithmatic Calculation

---
+++ 1. Basic Artithmetic/Practical - Arithmatic.R
@@ -0,0 +1,46 @@
+#Assign Variable (1)
+x = 5
+
+#Assign Variable (2)
+y <- 7
+
+#Print Data (1)
+print(x)
+#Print Data (2)
+x
+
+y
+
+#Get All Variable Which is in used
+ls()
+
+#Remove y Variable 
+rm(y)
+y
+
+#Variable Name Rule ( First Char must be alpha)
+x.1 <- 1
+x.1
+
+#Assign String
+yy <- "marin"
+yy
+
+#Arithmatic Operation
+11+2
+
+11*4
+
+y^2
+
+sqrt(y)
+
+y^(1/2)
+
+log(2)
+
+exp(y)
+
+log2(y)
+
+abs(-16)
